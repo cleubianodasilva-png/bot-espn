0f3e1b0911823c9198375382ac8a3d0a76686c50
cleubianodasilva-png <cleubianodasilva@gmail.com>
2026-07-23 23:08:28 -0300

    pwa: sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,10 @@
-const CACHE_NAME = 'greens-vip-v1';
-const assets = [
-  'painel.html',
-  'manifest.json',
-  'bot_espn_logo.png'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(assets))
-  );
+const CACHE_NAME = 'greens-vip-v2';
+const assets = ['painel.html', 'manifest.json', 'icon.jpg'];
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(assets)));
 });
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
-  );
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
 });
+self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
